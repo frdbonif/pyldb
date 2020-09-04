12b63b335e9ccece4ab51cd3d3aebe952f9ca03d
frdbonif <65509453+frdbonif@users.noreply.github.com>
2020-09-05 00:09:01 +0100

    Moved to staff version

--- pyldb/details.py
+++ pyldb/details.py
@@ -1,2 +1,2 @@
-wsdl_url = "https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01"  # noqa: E501
-service_url = "https://lite.realtime.nationalrail.co.uk/OpenLDBWS/ldb11.asmx"
+wsdl_url = "https://lite.realtime.nationalrail.co.uk/OpenLDBSVWS/wsdl.aspx?ver=2017-10-01"
+service_url = "https://lite.realtime.nationalrail.co.uk/OpenLDBSVWS/ldbsv12.asmx"
